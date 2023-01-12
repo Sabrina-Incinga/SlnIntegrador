eaacf0ac911855db80da8b863f824bf86b111453
Sabrina-Incinga <sabrinagincinga@gmail.com>
2023-01-12 17:47:13 -0300

    Clase base

--- Entidades/Entidades/Paciente.cs
+++ Entidades/Entidades/Paciente.cs
@@ -4,9 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Entidades
+namespace Entidades.Entidades
 {
-    public class Class1
+    public class Paciente
     {
     }
 }
