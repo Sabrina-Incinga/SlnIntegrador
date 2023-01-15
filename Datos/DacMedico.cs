73e81ccb85a0462cdc4dac5670c5c6e70ccf7f89
Sabrina-Incinga <sabrinagincinga@gmail.com>
2023-01-15 20:40:42 -0300

    DacMedico

--- Datos/DacMedico.cs
+++ Datos/DacMedico.cs
@@ -3,10 +3,40 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Entidades.Entidades;
 
 namespace Datos
 {
-    public class Class1
+    public class DacMedico
     {
+        public List<Medico> ListarTodos()
+        {
+            //TODO
+            return null;
+        }
+
+        public List<Medico> ListarPorEspecialidad(string especialidad)
+        {
+            //TODO
+            return null;
+        }
+
+        public int Insertar(Medico medico)
+        {
+            //TODO
+            return 0;
+        }
+
+        public int Eliminar(int id)
+        {
+            //TODO
+            return 0;
+        }
+
+        public Medico SelectById(int id)
+        {
+            //TODO
+            return null;
+        }
     }
 }
